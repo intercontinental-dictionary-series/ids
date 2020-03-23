964bb74001cbb342fabc9be721110b0410ce3d60
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-03-23 21:12:00 +0100

    Add code for multiple profiles based on transcription type

--- README.md
+++ README.md
@@ -18,15 +18,29 @@ Conceptlists in Concepticon:
 ![Glottolog: 98%](https://img.shields.io/badge/Glottolog-98%25-green.svg "Glottolog: 98%")
 ![Concepticon: 100%](https://img.shields.io/badge/Concepticon-100%25-brightgreen.svg "Concepticon: 100%")
 ![Source: 22%](https://img.shields.io/badge/Source-22%25-red.svg "Source: 22%")
+![BIPA: 19%](https://img.shields.io/badge/BIPA-19%25-red.svg "BIPA: 19%")
+![CLTS SoundClass: 19%](https://img.shields.io/badge/CLTS%20SoundClass-19%25-red.svg "CLTS SoundClass: 19%")
 
 - **Varieties:** 329
 - **Concepts:** 1,310
 - **Lexemes:** 453,975
 - **Sources:** 95
 - **Synonymy:** 1.28
+- **Invalid lexemes:** 0
+- **Tokens:** 4,156,818
+- **Segments:** 542 (440 BIPA errors, 439 CTLS sound class errors, 102 CLTS modified)
+- **Inventory size (avg):** 38.85
 
 ## Possible Improvements:
 
 
 
 - Entries missing sources: 352122/453975 (77.56%)
+
+# Contributors
+
+Name | GitHub user | Role
+--- | --- | ---
+Robert Forkel | @xrotwang | maintainer
+
+
--- lexibank_ids.py
+++ lexibank_ids.py
@@ -5,7 +5,16 @@ from pylexibank.forms import FormSpec
 from pylexibank.providers import clld
 from pylexibank.util import progressbar
 
-GLOTTOCODE_UPDATES = {"sana1281": "sana1298", "pray1239": "phai1238", "samr1245": "somr1240"}
+# Import for local segmentation
+import unicodedata
+from segments import Tokenizer, Profile
+from segments.tree import Tree
+
+GLOTTOCODE_UPDATES = {
+    "sana1281": "sana1298",
+    "pray1239": "phai1238",
+    "samr1245": "somr1240",
+}
 
 
 @attr.s
@@ -31,39 +40,92 @@ class Dataset(clld.CLLD):
     lexeme_class = CustomLexeme
 
     def cmd_makecldf(self, args):
+        # parse concepts
         ccode = {
-            x.attributes["ids_id"]: x.concepticon_id for x in self.conceptlists[0].concepts.values()
+            x.attributes["ids_id"]: x.concepticon_id
+            for x in self.conceptlists[0].concepts.values()
         }
 
         args.writer.add_sources()
 
+        # write languges
         for row in self.original_cldf["LanguageTable"]:
             args.writer.add_language(
                 ID=row["ID"],
                 Name=row["Name"],
-                Glottocode=GLOTTOCODE_UPDATES.get(row["Glottocode"], row["Glottocode"]),
+                Glottocode=GLOTTOCODE_UPDATES.get(
+                    row["Glottocode"], row["Glottocode"]
+                ),
             )
 
+        # write concepts
         for row in self.original_cldf["ParameterTable"]:
             args.writer.add_concept(
-                ID=row["ID"], Name=row.pop("Name"), Concepticon_ID=ccode[row["ID"]]
+                ID=row["ID"],
+                Name=row.pop("Name"),
+                Concepticon_ID=ccode[row["ID"]],
             )
 
+        # locally load tokenizers; when issue #197 on pylexibank, it might
+        # be removed (but consider the individual language profiles, such
+        # as for Pano languages)
+        def get_tokenizer(profile_):
+            profile_ = Profile.from_file(str(profile_), form="NFC")
+            default_spec = list(next(iter(profile_.graphemes.values())).keys())
+            for grapheme in ["^", "$"]:
+                if grapheme not in profile_.graphemes:
+                    profile_.graphemes[grapheme] = {
+                        k: None for k in default_spec
+                    }
+            profile_.tree = Tree(list(profile_.graphemes.keys()))
+            return Tokenizer(
+                profile=profile_, errors_replace=lambda c: "<{0}>".format(c)
+            )
+
+        def apply_tokenizer(form, tokenizer):
+            return tokenizer(
+                unicodedata.normalize("NFC", "^" + form + "$")
+            ).split()
+
+        tokenizers = {}
+        profile_dir = self.etc_dir / "profiles"
+        for profile in profile_dir.glob("*.tsv"):
+            tokenizers[profile.stem] = get_tokenizer(profile)
+
+        # Iterate over rows
         for row in progressbar(self.original_cldf["FormTable"]):
             row["Value"] = row.pop("Form")
             row["Language_ID"] = row.pop("Language_ID")
             row["Parameter_ID"] = row.pop("Parameter_ID")
             row["AlternativeValue"] = row.pop("alt_form")
             row["Transcription"] = (row.pop("transcription") or "").lower()
-            row["AlternativeTranscription"] = (row.pop("alt_transcription") or "").lower()
+            row["AlternativeTranscription"] = (
+                row.pop("alt_transcription") or ""
+            ).lower()
             del row["ID"]
             del row["Contribution_ID"]
-            args.writer.add_forms_from_value(
-                Language_ID=row["Language_ID"],
-                Parameter_ID=row["Parameter_ID"],
-                Value=row["Value"],
-                Source=row["Source"],
-                AlternativeValue=row["AlternativeValue"],
-                Transcription=row["Transcription"],
-                AlternativeTranscription=row["AlternativeTranscription"],
-            )
+
+            # Add forms
+            # TODO: use AlternativeTranscription if it is `phonemic`
+            for i, form in enumerate(
+                self.form_spec.split({}, row["Value"], {})
+            ):
+                if row["Transcription"] in tokenizers:
+                    segments = apply_tokenizer(
+                        form, tokenizers[row["Transcription"]]
+                    )
+                else:
+                    segments = [c for c in form]
+
+                # Add forms
+                args.writer.add_form_with_segments(
+                    Language_ID=row["Language_ID"],
+                    Parameter_ID=row["Parameter_ID"],
+                    Value=row["Value"],
+                    Form=form,
+                    Source=row["Source"],
+                    AlternativeValue=row["AlternativeValue"],
+                    Transcription=row["Transcription"],
+                    AlternativeTranscription=row["AlternativeTranscription"],
+                    Segments=segments,
+                )
