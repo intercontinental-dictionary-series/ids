5bdc9c506b7de109fef06a24b6ca771bde5696e6
Hans-Jörg Bibiko <hans_joerg_bibiko@eva.mpg.de>
2021-09-08 11:53:07 +0200

    read in given edited data (via xlsx/md files after download cmd)

--- lexibank_ids.py
+++ lexibank_ids.py
@@ -3,6 +3,7 @@ import pathlib
 import re
 import unicodedata
 from collections import defaultdict
+from datetime import datetime
 from itertools import groupby
 from urllib.request import urlretrieve
 from zipfile import ZipFile
@@ -113,20 +114,54 @@ class Dataset(IDSDataset):
         ids_lgs = [lg for lg in self.ids_raw_read("lang") if lg.status == "0"]
         ids_lgs_ids = [lg.lg_id for lg in ids_lgs]
 
+        edited_data = {}
+        edited_path = self.raw_dir / 'edited'
+        for edited_data_filename in edited_path.glob('*.csv'):
+            ed_lg_id = re.match(r'^.+?\-(\d+)', edited_data_filename.stem).group(1)
+            if ed_lg_id not in ids_lgs_ids:
+                args.log.warn('ID {} not known for edited data file {}'.format(
+                    ed_lg_id, edited_data_filename))
+                continue
+            edited_data[ed_lg_id] = {}
+            edited_data[ed_lg_id]['file'] = edited_data_filename
+            edited_data[ed_lg_id]['md'] = edited_data_filename.with_suffix('.md')
+            try:
+                edited_data[ed_lg_id]['date'] = datetime.fromtimestamp(
+                    edited_data_filename.with_suffix('.xlsx').stat().st_mtime).strftime('%Y-%m-%d')
+            except FileNotFoundError:
+                edited_data[ed_lg_id]['date'] = datetime.fromtimestamp(
+                    edited_data_filename.stat().st_mtime).strftime('%Y-%m-%d')
+
         sources = defaultdict(list)
         for lc in self.ids_raw_read("lang_compilers"):
             if lc.lg_id not in ids_lgs_ids or lc.name == "BIBIKO":
                 continue
             lname = SOURCE_UPDATES.get(lc.name, lc.name)
-            if lc.what_did_id in ["1", "2", "3"]:
-                personnel[lc.what_did_id][lc.lg_id].append(lname)
+            if lc.lg_id in edited_data:
+                p = self.get_personnel(args, edited_data[lc.lg_id]['md'])
+                personnel["1"][lc.lg_id] = p['data entry']
+                personnel["2"][lc.lg_id] = p['author']
+                personnel["3"][lc.lg_id] = p['consultant']
             else:
-                assert int(lc.what_did_id) in [4, 395]
-                sources[lc.lg_id].append(lname.lower())
+                if lc.what_did_id in ["1", "2", "3"]:
+                    personnel[lc.what_did_id][lc.lg_id].append(lname)
+                else:
+                    assert int(lc.what_did_id) in [4, 395]
+                    sources[lc.lg_id].append(lname.lower())
 
         data_desc = defaultdict(dict)
         for ld in self.ids_raw_read("x_lg_data"):
-            data_desc[ld.lg_id.strip()][ld.map_ids_data] = ld.header.strip()
+            lid = ld.lg_id.strip()
+            if lid in edited_data:
+                edited_data[lid]['data'] = edited_path.read_csv(edited_data[lid]['file'], dicts=True)
+                header = [h for h in edited_data[lid]['data'][0].keys()
+                          if h.strip().lower() not in ['chapter', 'entry', 'english', 'comment']]
+                edited_data[lid]['reprs'] = []
+                for i, h in enumerate(header):
+                    data_desc[lid][str(i + 1)] = h.strip()
+                    edited_data[lid]['reprs'].append(h.strip())
+            else:
+                data_desc[lid][ld.map_ids_data] = ld.header.strip()
 
         altnames = defaultdict(list)
         for n in self.ids_raw_read("alt_names"):
@@ -138,9 +173,9 @@ class Dataset(IDSDataset):
         glottolog_codes = self.glottolog.languoids_by_code()
 
         # get ISO language ID mapping
-        iso_codes = {lg.id.strip(): lg.sil_code.strip() for lg in self.ids_raw_read("sil_lang")}
+        _iso_codes = {lg.id.strip(): lg.sil_code.strip() for lg in self.ids_raw_read("sil_lang")}
         iso_codes = {
-            lg.lg_id.strip(): iso_codes[lg.sil_id.strip()] for lg in self.ids_raw_read("x_lg_sil")
+            lg.lg_id.strip(): _iso_codes[lg.sil_id.strip()] for lg in self.ids_raw_read("x_lg_sil")
         }
 
         lang_corrections = defaultdict(dict)
@@ -162,10 +197,15 @@ class Dataset(IDSDataset):
             if not gl and iso:
                 gl = glottolog_codes[iso].id
             gl = GLOTTOCODE_UPDATES.get(gl, gl)
-            reprs = [data_desc[lg_id].get("1")]
-            if data_desc[lg_id].get("2", ""):
-                if lg_id != "194":  # ignore 2. repr - it's in comment
-                    reprs.append(data_desc[lg_id].get("2"))
+            date = lg.date.strip()
+            if lg_id in edited_data:
+                reprs = edited_data[lg_id]['reprs']
+                date = edited_data[lg_id]['date']
+            else:
+                reprs = [data_desc[lg_id].get("1")]
+                if data_desc[lg_id].get("2", ""):
+                    if lg_id != "194":  # ignore 2. repr - it's in comment
+                        reprs.append(data_desc[lg_id].get("2"))
             args.writer.add_language(
                 ID=lg_id,
                 Name=lg_name.strip(),
@@ -178,7 +218,7 @@ class Dataset(IDSDataset):
                 Latitude=glottolog_codes[gl].latitude if gl else "",
                 Longitude=glottolog_codes[gl].longitude if gl else "",
                 alt_names=sorted(set(altnames[lg_id]) - {lg_name.strip()}) or "",
-                date=lg.date.strip(),
+                date=date,
             )
 
         args.writer.objects["LanguageTable"] = sorted(
@@ -215,6 +255,32 @@ class Dataset(IDSDataset):
             words = defaultdict(list)
             lg_org_value = defaultdict(str)
             entries_list = list(entries)
+
+            if lg_id in edited_data:
+                for row in edited_data[lg_id]['data']:
+
+                    entry_id = "{0}-{1}".format(row['Chapter'], row['Entry'])
+                    if entry_id not in entry_ids:
+                        continue
+                    lg_entry_id = "{0}-{1}".format(lg_id, entry_id)
+                    reprs = edited_data[lg_id]['reprs']
+                    w = row[reprs[0]].strip()
+                    if w and not empty.match(w):
+                        w = unicodedata.normalize(self.form_spec.normalize_unicode, w)
+                        alt_vals = []
+                        for r in reprs[1:]:
+                            alt_vals.append(row[r].strip())
+                        args.writer.add_form(
+                            Language_ID=lg_id,
+                            Parameter_ID=entry_id,
+                            Value=w,
+                            Form=w,
+                            Comment=row['Comment'].strip(),
+                            Source=sources.get(lg.lg_id, ""),
+                            Transcriptions=reprs,
+                            AlternativeValues=alt_vals,
+                        )
+                continue
 
             # find last valid entry
             last_entry_idx = None
